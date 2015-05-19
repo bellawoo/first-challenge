b8577328e7bb8d5a6bb12da81c0a432dcdda1f46
Bella <woo.bella@gmail.com>
2015-05-19 10:33:33 -0400

    answers in challenge.rb file

--- challenge.rb
+++ challenge.rb
@@ -6,29 +6,34 @@
 
 # 1. Read this file. What do you expect it to do when
 #    you run it?
+# This should count how many A's and Z's there rae in the fox.txt file.
 # 2. Now run it. Are there any bugs?
+# The file ran without any errors, but it did not actually put out the results.
 # 3. Can you make any improvements to the program?
 # 4. How many Q's are there in "prufrock.txt"?
+# 6 Q's are in prufrock.txt.
 # 5. (Harder) What are the 5 most common letters in "prufrock.txt"?
+# e, a, t, o, i
 
 # When you're done (at least with 4), commit your work,
 # push it to your repo, and open a pull request.
 
 require 'pry'
 
-text = File.read "fox.txt"
+text = File.read("prufrock.txt").downcase
 
 cs = Hash.new
 text.chars.each do |l|
   if cs[l]
-    cs[l] = cs[l] + 1
+    cs[l] += 1
   else
     cs[l] = 1
   end
 end
-
-ac = cs[:a]
-zc = cs[:z]
+binding.pry
+ac = cs["a"]
+zc = cs["z"]
+qc = cs["q"]
 
 #puts "Text is: #{text}"
-puts "There are #{ac} A's and #{zc} Z's"
+puts "There are #{ac} A's and #{zc} Z's and #{qc} Q's"
